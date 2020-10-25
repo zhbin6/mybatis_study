a50f93ae92be100b78c6bab5e9250c21585a3e86
zhbin6 <710533544@qq.com>
2020-10-25 21:39:22 +0800

    6s

--- mybatis-04/src/main/java/com/zhbin/dao/UserMapper.java
+++ mybatis-04/src/main/java/com/zhbin/dao/UserMapper.java
@@ -2,6 +2,10 @@ package com.zhbin.dao;
 
 import com.zhbin.pojo.User;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserMapper {
     public User getUserById(int id);
+    public List<User> getUserByLimit(Map<String,Integer> map);
 }
--- mybatis-04/src/test/java/com/zhbin/dao/UserMapperTest.java
+++ mybatis-04/src/test/java/com/zhbin/dao/UserMapperTest.java
@@ -6,17 +6,16 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
-//import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UserMapperTest {
 
-    static Logger logger = Logger.getLogger(UserMapperTest.class);
+//    static Logger logger = Logger.getLogger(UserMapperTest.class);
 
     @Test
     public void getUserById(){
-
-
-
         SqlSession sqlSession = null;
         try {
             sqlSession = MybatisUtil.getSqlSession();
@@ -29,14 +28,33 @@ public class UserMapperTest {
         finally {
             sqlSession.close();
         }
-
-
     }
+//    @Test
+//    public void Log4jTest(){
+//        logger.info("info:come in log4j");
+//        logger.debug("debug:come in debug");
+//        logger.error("error:come in error");
+//    }
+
     @Test
-    public void Log4jTest(){
-        logger.info("info:come in log4j");
-        logger.debug("debug:come in debug");
-        logger.error("error:come in error");
+    public void getUserByLimit(){
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtil.getSqlSession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            Map<String,Integer> map = new HashMap<String, Integer>();
+            map.put("startIndex",0);
+            map.put("pageSize",2);
+            List<User> users = userMapper.getUserByLimit(map);
+            for (User user : users) {
+                System.out.println(user);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            sqlSession.close();
+        }
     }
 
 }
